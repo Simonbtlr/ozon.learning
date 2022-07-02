4d19b5b81185134f4408ca5ca6d9d3a8777fe2e1
Arthur <simongamezs@gmail.com>
2022-07-03 00:29:20 +0300

    add arguments.sh

---
+++ Frontend-разработка/Факультативный материал по Bash-скриптам/functions/arguments.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env sh
+
+sum_two_numbers() {
+    local first
+    local second
+
+    first="$1"
+    second="$2"
+
+    echo "$((first + second))"
+}
+
+sum_two_numbers 2 3
+
+# Default values:
+greet() {
+    local user
+    user="${1:-root}"
+
+    echo "Hello, $user"
+}
+
+greet Simon # Hello, Simon
+greet       # Hello, root
+
+# Named arguments:
+with_named() {
+    local h
+    local p
+    local d
+
+    OPTIND=1
+
+    while getopts 'hd:p:' opt; do
+        case "$opt" in
+            h) h=$OPTARG;;
+            p) p=$OPTARG;;
+            d) d=$OPTARG;;
+
+            *) (echo 'invalid arg'; exit 1);;
+        esac
+    done
+
+    shift $((OPTIND-1))
+    [ "$1" = '--' ] && shift
+
+    echo "
+    ARGS:
+    - h: $h
+    - p: $p
+    - d: $d
+    "
+}
+
+with_named -p 'text'
+with_named -h -d 1 -p 'text'
+with_named -e 1
